66143e95febe7651394e2e1193e948d33445e193
Kim-TaeGwan <ab154@naver.com>
2023-12-31 03:31:38 +0900

    first commit

---
+++ src/main/java/org/example/riotspring/RiotSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.riotspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiotSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiotSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/riotspring/RiotSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.riotspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiotSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
