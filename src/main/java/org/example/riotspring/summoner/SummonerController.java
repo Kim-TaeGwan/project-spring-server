2449b2a72654d3b7d1d1bec286af8c853f1bc2c4
Kim-TaeGwan <ab154@naver.com>
2024-01-07 00:32:26 +0900

    feat: summoners api 개발

--- src/main/java/org/example/riotspring/summoner/SummonerController.java
+++ src/main/java/org/example/riotspring/summoner/SummonerController.java
@@ -1,14 +1,38 @@
 package org.example.riotspring.summoner;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.riotspring.RiotAPIService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/summoners")
 public class SummonerController {
-    @GetMapping("/summoner")
-    public String summoner(){
-        return "summoner";
+
+    private final RiotAPIService riotAPIService;
+    private final ObjectMapper objectMapper;
+
+    public SummonerController(RiotAPIService riotAPIService, ObjectMapper objectMapper){
+        this.riotAPIService = riotAPIService;
+        this.objectMapper = objectMapper;
+    }
+
+    @GetMapping("/by-name")
+    public ResponseEntity<SummonerDTO> summonerData(@RequestParam("name") String summonerName) {
+        String response = riotAPIService.callRiotAPI("/lol/summoner/v4/summoners/by-name/"+ summonerName);
+        System.out.println("summonerData : " + response);
+        if (response != null) {
+            try {
+                SummonerDTO summonerDTO = objectMapper.readValue(response, SummonerDTO.class);
+                return ResponseEntity.ok(summonerDTO);
+            } catch (Exception e) {
+                // JSON 파싱 예외 처리
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+        } else {
+            // 다른 상태 코드 처리
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
---
+++ src/main/java/org/example/riotspring/summoner/SummonerDTO.java
@@ -0,0 +1,15 @@
+package org.example.riotspring.summoner;
+
+import lombok.Data;
+
+@Data
+public class SummonerDTO {
+ private String id;
+ private String accountId;
+ private Integer profileIconId;
+ private Long revisionDate;
+ private String name;
+ private String puuid;
+ private Long summonerLevel;
+
+}
